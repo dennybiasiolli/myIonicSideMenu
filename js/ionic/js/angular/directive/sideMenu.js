85e77c59e8f76b4dc33f1994d59a819005109ab2
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-07-11 13:04:51 +0200

    customized ionic sideMenu with leave-content-active attribute

---
+++ js/ionic/js/angular/directive/sideMenu.js
@@ -0,0 +1,65 @@
+/**
+ * @ngdoc directive
+ * @name ionSideMenu
+ * @module ionic
+ * @restrict E
+ * @parent ionic.directive:ionSideMenus
+ *
+ * @description
+ * A container for a side menu, sibling to an {@link ionic.directive:ionSideMenuContent} directive.
+ *
+ * @usage
+ * ```html
+ * <ion-side-menu
+ *   side="left"
+ *   width="myWidthValue + 20"
+ *   is-enabled="shouldLeftSideMenuBeEnabled()">
+ * </ion-side-menu>
+ * ```
+ * For a complete side menu example, see the
+ * {@link ionic.directive:ionSideMenus} documentation.
+ *
+ * @param {string} side Which side the side menu is currently on.  Allowed values: 'left' or 'right'.
+ * @param {boolean=} is-enabled Whether this side menu is enabled.
+ * @param {number=} width How many pixels wide the side menu should be.  Defaults to 275.
+ */
+angular.module('ionic')
+.directive('ionSideMenu2', function() {
+  return {
+    restrict: 'E',
+    require: '^ionSideMenus2',
+    scope: true,
+    compile: function(element, attr) {
+      angular.isUndefined(attr.isEnabled) && attr.$set('isEnabled', 'true');
+      angular.isUndefined(attr.width) && attr.$set('width', '275');
+      angular.isUndefined(attr.leaveContentActive) && attr.$set('leaveContentActive', 'false');
+
+      element.addClass('menu menu-' + attr.side);
+
+      return function($scope, $element, $attr, sideMenuCtrl) {
+        $scope.side = $attr.side || 'left';
+
+        var sideMenu = sideMenuCtrl[$scope.side] = new ionic.views.SideMenu2({
+          width: attr.width,
+          type: attr.type,
+          leaveContentActive: false,
+          el: $element[0],
+          isEnabled: true
+        });
+
+        $scope.$watch($attr.width, function(val) {
+          var numberVal = +val;
+          if (numberVal && numberVal == val) {
+            sideMenu.setWidth(+val);
+          }
+        });
+        $scope.$watch($attr.isEnabled, function(val) {
+          sideMenu.setIsEnabled(!!val);
+        });
+        $scope.$watch($attr.leaveContentActive, function(val) {
+          sideMenu.setLeaveContentActive(!!val);
+        });
+      };
+    }
+  };
+});
