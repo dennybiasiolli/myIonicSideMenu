9fe19ee042e38b23831070515a12a8d9c530ca02
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-07-19 14:55:42 +0200

    using custom ionic bundle
    take a look at https://github.com/dennybiasiolli/ionic/tree/1.x_denny

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 angular.module('ionicApp', ['ionic'])
-    .controller('MainCtrl', function($scope, $window, $ionicSideMenuDelegate) {
-
+    .controller('MainCtrl', function($scope, $window, $ionicSideMenuDelegate, $element, $timeout) {
         $scope.width = function() {
             return $window.innerWidth;
         };
 
-        $scope.toggleLeft = function() {
-            $ionicSideMenuDelegate.toggleLeft();
-        };
-
-        $scope.toggleRight = function() {
-            $ionicSideMenuDelegate.toggleRight();
+        $scope.first = true;
+        $scope.testAnimation = function() {
+            $element.find('ion-side-menu-content').addClass('menu-animated');
+            angular.element($element.find('ion-side-menu')[0]).css('webkit-transform', 'translate3d(' + ($scope.first ? '50px' : '0') + ', 0, 0)');
+            angular.element($element.find('ion-side-menu')[1]).css('webkit-transform', 'translate3d(' + ($scope.first ? '-100px' : '0') + ', 0, 0)');
+            $element.find('ion-side-menu-content').css('webkit-transform', 'translate3d(0, ' + ($scope.first ? '100px' : '0') + ', 0)');
+            $scope.first = !$scope.first;
         };
     });
